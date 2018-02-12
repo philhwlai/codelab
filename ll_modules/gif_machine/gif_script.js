847d9880623f254cbf40fac6a4a8b042e17f71b6
mk <mk@mks-MacBook-Pro.local>
2018-02-11 23:44:19 -0500

    fixed link

---
+++ ll_modules/gif_machine/gif_script.js
@@ -0,0 +1,15 @@
+
+
+ffmpeg -i mkmermaid.mov -vf palettegen mkmermaid-palette.png
+
+
+ffmpeg -i mkmermaid.mov -i mkmermaid-palette.png -vf scale=640:360 -y mkmermaid360.gif
+
+ffmpeg -i mkmermaid.mov -i mkmermaid-palette.png -vf scale=960:540 -y mkmermaid540.gif
+
+
+ffmpeg -i mkmermaid.mov -filter:v "crop=720:1080:600:0" -c:a copy out.mp4
+
+
+
+ffmpeg -i out.mp4 -i mkmermaid-palette.png -vf scale=480:720 -y mkmermaid720.gif
