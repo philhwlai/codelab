b456537e8a785f70027bc7ece0efcb2033031ee1
mk <mk@mks-MacBook-Pro.local>
2018-02-12 00:03:49 -0500

    link fix

--- ll_modules/gif_machine/gif_script.js
+++ ll_modules/gif_machine/gif_script.js
@@ -13,3 +13,8 @@ ffmpeg -i mkmermaid.mov -filter:v "crop=720:1080:600:0" -c:a copy out.mp4
 
 
 ffmpeg -i out.mp4 -i mkmermaid-palette.png -vf scale=480:720 -y mkmermaid720.gif
+
+
+ffmpeg -i plane_gif.mov -vf palettegen palette.png
+
+ffmpeg -i plane_gif.mov -i palette.png -vf scale=640:360 -y plane_gif360.gif
