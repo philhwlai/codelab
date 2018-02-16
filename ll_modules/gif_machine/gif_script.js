e62d6e82fa945ff4b08017f051a15e7b404c0f4f
mk <mk@mks-MacBook-Pro.local>
2018-02-15 22:57:02 -0500

    youtube demo added

--- ll_modules/gif_machine/gif_script.js
+++ ll_modules/gif_machine/gif_script.js
@@ -1,20 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+const cp = require('child_process');
+
+var filepath =
+var palettePath =
+
+function makeGif(filepath){
+  cp.spawnSync('ffpmeg', '-i', filepath, '-vf', 'palettegen',
+    palettePath);
+  cp.spawnSync('ffpmeg', '-i', filepath, '-vf', 'palettegen',
+    palettePath);
+  // fs remove palette
+}
 
 
-ffmpeg -i mkmermaid.mov -vf palettegen mkmermaid-palette.png
 
 
-ffmpeg -i mkmermaid.mov -i mkmermaid-palette.png -vf scale=640:360 -y mkmermaid360.gif
-
-ffmpeg -i mkmermaid.mov -i mkmermaid-palette.png -vf scale=960:540 -y mkmermaid540.gif
-
-
-ffmpeg -i mkmermaid.mov -filter:v "crop=720:1080:600:0" -c:a copy out.mp4
-
-
-
-ffmpeg -i out.mp4 -i mkmermaid-palette.png -vf scale=480:720 -y mkmermaid720.gif
-
-
-ffmpeg -i plane_gif.mov -vf palettegen palette.png
-
-ffmpeg -i plane_gif.mov -i palette.png -vf scale=640:360 -y plane_gif360.gif
+// ffmpeg -i mkmermaid.mov -vf palettegen mkmermaid-palette.png
+//
+//
+// ffmpeg -i mkmermaid.mov -i mkmermaid-palette.png -vf scale=640:360 -y mkmermaid360.gif
+//
+// ffmpeg -i mkmermaid.mov -i mkmermaid-palette.png -vf scale=960:540 -y mkmermaid540.gif
+//
+//
+// ffmpeg -i mkmermaid.mov -filter:v "crop=720:1080:600:0" -c:a copy out.mp4
+//
+//
+//
+// ffmpeg -i out.mp4 -i mkmermaid-palette.png -vf scale=480:720 -y mkmermaid720.gif
+//
+//
+// ffmpeg -i plane_gif.mov -vf palettegen palette.png
+//
+// ffmpeg -i plane_gif.mov -i palette.png -vf scale=640:360 -y plane_gif360.gif
---
+++ public/_tests/three/js/primitives.js
@@ -0,0 +1,251 @@
+var GEO_TYPES = ['cone', 'cylinder', 'octahedron', 'sphere', 'tetrahedron', 'torus', 'torusKnot'];
+
+function init() {
+  var scene = new THREE.Scene();
+  var gui = new dat.GUI();
+  var clock = new THREE.Clock();
+
+  // initialize objects in the scene
+  var geoTypes = GEO_TYPES;
+  var objMaterial = getMaterial('standard', 'rgb(255,255,255)');
+
+  var sphereMaterial = getMaterial('standard', 'rgb(240,240,240)');
+  var sphere = getSphere(sphereMaterial, 1, 48);
+
+  var planeMaterial = getMaterial('standard', 'rgb(255,255,255)');
+  var plane = getPlane(planeMaterial, 100);
+
+  var lightLeft = getSpotLight(1, 'rgb(255, 220, 180)');
+  var lightRight = getSpotLight(1, 'rgb(255, 220, 180)');
+
+  var objMaterial = getMaterial('basic', 'rgb(240,240,240)');
+  var obj = getGeometry('torus', 1, objMaterial);
+  console.log(obj);
+
+  scene.add(lightRight);
+  scene.add(lightLeft);
+  scene.add(plane);
+  scene.add(obj);
+  // scene.add(sphere);
+  // manipluate objects
+
+  sphere.position.y = sphere.geometry.parameters.radius;
+  plane.rotation.x = Math.PI/2;
+
+  lightLeft.position.x = -5;
+  lightLeft.position.y = 2;
+  lightLeft.position.z = -4;
+
+  lightRight.position.x = 5;
+  lightRight.position.y = 2;
+  lightRight.position.z = -4;
+
+  // manipulate materials
+  // load cube map
+
+  var path = '/images/cubemaps/swedish_castle/';
+  var format = '.jpg';
+  var urls = [
+      path + 'px' + format,
+      path + 'nx' + format,
+      path + 'py' + format,
+      path + 'ny' + format,
+      path + 'pz' + format,
+      path + 'nz' + format,
+  ];
+  // var urls = [
+  //     path + 'bk' + format,
+  //     path + 'dn' + format,
+  //     path + 'ft' + format,
+  //     path + 'lf' + format,
+  //     path + 'rt' + format,
+  //     path + 'up' + format
+  // ];
+  var reflectionCube = new THREE.CubeTextureLoader().load(urls);
+  reflectionCube.format = THREE.RGBFormat;
+
+  scene.background = reflectionCube;
+
+  var loader = new THREE.TextureLoader();
+  planeMaterial.map = loader.load('/images/concrete_002.jpg');
+  planeMaterial.bumpMap = loader.load('/images/concrete_002.jpg');
+  planeMaterial.roughnessMap = loader.load('/images/concrete_002.jpg');
+  planeMaterial.bumpScale = 0.01;
+  planeMaterial.metalness = 0.1;
+  planeMaterial.roughness = 0.7;
+  planeMaterial.envMap = reflectionCube;
+
+  sphereMaterial.roughnessMap = loader.load('/images/fingerprints_001.jpg');
+  sphereMaterial.roughness = 1;
+  sphereMaterial.envMap = reflectionCube;
+
+
+  var maps = ['map', 'bumpMap'];
+  maps.forEach(function(mapName){
+    var texture = planeMaterial[mapName];
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set(30, 30);
+  })
+
+  var camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth/window.innerHeight,
+    1,
+    1000
+  );
+  scene.add(camera);
+  camera.position.z = 15;
+  camera.position.y = 5;
+
+  //dat.gui
+
+  var folder1 = gui.addFolder('light_1');
+  folder1.add(lightLeft, 'intensity', 0, 10);
+  folder1.add(lightLeft.position, 'x', -5, 15);
+  folder1.add(lightLeft.position, 'y', -5, 15);
+  folder1.add(lightLeft.position, 'z', -5, 15);
+
+  var folder4 = gui.addFolder('primitives');
+  folder4.add(obj, 'name');
+
+  // var folder2 = gui.addFolder('light_2');
+  // folder2.add(lightRight, 'intensity', 0, 10);
+  // folder2.add(lightRight.position, 'x', -5, 15);
+  // folder2.add(lightRight.position, 'y', -5, 15);
+  // folder2.add(lightRight.position, 'z', -5, 15);
+  //
+  // var folder3 = gui.addFolder('materials');
+  // folder3.add(sphereMaterial, 'roughness', 0, 1);
+  // folder3.add(planeMaterial, 'roughness', 0, 1);
+  // folder3.add(sphereMaterial, 'metalness', 0, 1);
+  // folder3.add(planeMaterial, 'metalness', 0, 1);
+  // gui.add(camera.position, 'z', 10, 20);
+
+  var renderer = new THREE.WebGLRenderer();
+  renderer.shadowMap.enabled = true;
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setClearColor(0xffffff);
+  document.getElementById('webgl').appendChild(renderer.domElement);
+
+  var controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+  update(renderer, scene, camera, controls, clock);
+
+  return scene;
+
+}
+
+function getSphere(material, size, segments) {
+  var geometry = new THREE.SphereGeometry(size, segments, segments);
+  var obj = new THREE.Mesh(geometry, material);
+  obj.castShadow = true;
+  return obj;
+}
+
+function getPlane(material, size) {
+  var geometry = new THREE.PlaneGeometry(size, size);
+  material.side = THREE.DoubleSide;
+  var obj = new THREE.Mesh(geometry, material);
+  obj.receiveShadow = true;
+  return obj;
+}
+
+function getGeometry(type, size, material){
+  var geometry;
+  var segmentMultiplier = 1;
+
+  switch (type) {
+    case 'box':
+      geometry = new THREE.BoxGeometry(size, size, size);
+      break;
+    case 'cone':
+      geometry = new THREE.ConeGeometry(size, size, 256*segmentMultiplier);
+      break;
+    case 'cylinder':
+      geometry = new THREE.CylinderGeometry(size, size, size, 32*segmentMultiplier);
+      break;
+    case 'octahedron':
+      geometry = new THREE.OctahedronGeometry(size);
+      break;
+    case 'sphere':
+      geometry = new THREE.SphereGeometry(size, 32*segmentMultiplier, 32*segmentMultiplier);
+      break;
+    case 'tetrahedron':
+      geometry = new THREE.TetrahedronGeometry(size);
+      break;
+    case 'torus':
+      geometry = new THREE.TorusGeometry(size/2, size/4, 16*segmentMultiplier, 100*segmentMultiplier);
+      break;
+    case 'torusKnot':
+      geometry = new THREE.TorusKnotGeometry(size/2, size/6, 256*segmentMultiplier, 100*segmentMultiplier);
+      break;
+    default:
+      break;
+  }
+
+  var obj = new THREE.Mesh(geometry, material);
+  obj.castShadow = true;
+  obj.name = type;
+
+  return obj;
+}
+
+function getMaterial(type, color) {
+  var selectedMaterial;
+  var materialOptions = {
+    color: color === undefined ? 'rgb(255, 255, 255)' : color,
+    wireframe: true
+  };
+
+  switch (type) {
+    case 'basic':
+        selectedMaterial = new THREE.MeshBasicMaterial(materialOptions);
+        break;
+    case 'lambert':
+        selectedMaterial = new THREE.MeshLambertMaterial(materialOptions);
+        break;
+    case 'phong':
+        selectedMaterial = new THREE.MeshPhongMaterial(materialOptions);
+        break;
+    case 'standard':
+        selectedMaterial = new THREE.MeshStandardMaterial(materialOptions);
+        break;
+    default:
+        selectedMaterial = new THREE.MeshBasicMaterial(materialOptions);
+        break;
+  }
+
+  return selectedMaterial;
+}
+
+function getSpotLight(intensity, color) {
+  color = color === undefined ? 'rgb(255, 255, 255)' : color;
+  var light = new THREE.SpotLight(color, intensity);
+  light.castShadow = true;
+  light.penumbra = .5;
+
+  // set up shadow properties for light
+  light.shadow.mapSize.width = 2048;
+  light.shadow.mapSize.height = 2048;
+  light.shadow.bias = 0.001;
+
+  return light;
+}
+
+function update(renderer, scene, camera, controls, clock){
+  renderer.render(scene, camera);
+  controls.update();
+  var timeElapsed = clock.getElapsedTime();
+  // console.log(timeElapsed);
+  // var cameraYRotation = scene.getObjectByName('cameraYRotation');
+  // if (cameraYRotation.rotation.y < .9) {
+  //   cameraYRotation.rotation.y += 0.0005;
+  // }
+  // cameraYRotation.rotation.y += 0.0009;
+  requestAnimationFrame(function(){
+    update(renderer, scene, camera, controls, clock);
+  })
+}
+
+init();
